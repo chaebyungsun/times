8d6c2b794e858e62f6985a1185e8a3dfa21afcba
chaebyungsun <rhemdgkrtod3@naver.com>
2024-07-10 03:20:06 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const apiKey = `6c9ebc66510b40e3b8f76a56e9af9fb2`;
 let news = [];
 
 const getLatestNews = async () => {
-  const url = new URL(`https://times-sun.netlify.app/top-headlines`);
+  let url = new URL(`https://times-sun.netlify.app/top-headlines`);
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
